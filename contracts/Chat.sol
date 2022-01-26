87b7fc5af3eff4815fedcad0da4b715e2909cc8a
Matt <matt@Matts-MacBook-Pro.local>
2022-01-26 16:20:24 +0100

    Add sendMsg func to send plaintext data

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -10,19 +10,23 @@ contract Chat is Customizable {
     using Counters for Counters.Counter;
 
     event MsgSent(uint256 indexed id, address indexed sender);
+    event MemberAdded(address indexed member);
+    event MemberRemoved(address indexed member);
 
     struct Message {
         uint256 id;
         uint256 time;
-        string content;
         uint256 replyTo;
         address sender;
+        string data;
+        bool encrypted;
     }
 
     EnumerableSet.AddressSet members;
 
     Counters.Counter public idCounter;
     mapping(uint256 => Message) public messages;
+    mapping(bytes32 => bytes) public messagesCipher;
 
     modifier onlyMember() {
         require(members.contains(msg.sender));
@@ -44,17 +48,57 @@ contract Chat is Customizable {
         _transferOwnership(owner);
     }
 
-    function sendMsg(string memory content, uint256 replyTo) public onlyMember {
+    function sendMsg(string memory data, uint256 replyTo) public onlyMember {
         idCounter.increment();
         uint256 id = idCounter.current();
 
         Message memory message;
         message.id = id;
         message.time = block.timestamp;
-        message.content = content;
         message.replyTo = replyTo;
         message.sender = msg.sender;
+        message.data = data;
 
         messages[id] = message;
+
+        emit MsgSent(id, msg.sender);
+    }
+
+    function sendCipherMsg(bytes[] memory ciphers, uint256 replyTo)
+        public
+        onlyMember
+    {
+        require(members.length() == ciphers.length);
+
+        idCounter.increment();
+        uint256 id = idCounter.current();
+
+        Message memory message;
+        message.id = id;
+        message.time = block.timestamp;
+        message.replyTo = replyTo;
+        message.sender = msg.sender;
+        message.encrypted = true;
+
+        messages[id] = message;
+        for (uint256 i = 0; i < ciphers.length; i++) {
+            messagesCipher[
+                keccak256(abi.encodePacked(id, members.at(i)))
+            ] = ciphers[i];
+        }
+
+        emit MsgSent(id, msg.sender);
+    }
+
+    function addMember(address member) public onlyOwner {
+        require(members.add(member));
+
+        emit MemberAdded(member);
+    }
+
+    function removeMember(address member) public onlyOwner {
+        require(members.remove(member));
+
+        emit MemberRemoved(member);
     }
 }
