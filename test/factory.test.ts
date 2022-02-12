9aec78aa7466505f897e1a5aa9695d0b593d1468
Matt <matt@Matts-MacBook-Pro.local>
2022-02-12 13:25:45 +0100

    Add create/remove chat/profile tests

--- test/factory.test.ts
+++ test/factory.test.ts
@@ -1,41 +1,55 @@
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Factory", function () {
-  it("Should create new Chat", async function () {
-    const Factory = await ethers.getContractFactory("Factory");
-    const factory = await Factory.deploy();
-    await factory.deployed();
+  const chatId = ethers.utils.id("example-id");
+  let factory: Contract;
 
+  it("Should deploy factory", async function () {
+    const Factory = await ethers.getContractFactory("Factory");
+    factory = await Factory.deploy();
+    await factory.deployed();
+  });
+
+  it("Should create chat", async function () {
     const [signer] = await ethers.getSigners();
     const members = [signer.address];
 
-    await (await factory.createChat(members)).wait();
+    await (await factory.createChat(chatId, members)).wait();
 
-    const chatAddress = await factory.chats(
-      ethers.utils.keccak256(
-        ethers.utils.defaultAbiCoder.encode(["address"], members)
-      )
-    );
+    const chatAddress = await factory.chats(chatId);
     expect(chatAddress).to.not.equal(ethers.constants.AddressZero);
-
-    await expect(factory.createChat(members)).to.be.reverted;
   });
 
-  it("Should create new Profile", async function () {
-    const Factory = await ethers.getContractFactory("Factory");
-    const factory = await Factory.deploy();
-    await factory.deployed();
+  it("Should revert if id chat is taken", async function () {
+    const [signer] = await ethers.getSigners();
+    const members = [signer.address];
 
+    await expect(factory.createChat(chatId, members)).to.be.reverted;
+  });
+
+  it("Should revert remove function if signer is not chat's owner", async function () {
+    const [, signer] = await ethers.getSigners();
+
+    await expect(factory.connect(signer).removeChat(chatId)).to.be.reverted;
+  });
+
+  it("Should remove chat", async function () {
+    await (await factory.removeChat(chatId)).wait();
+
+    const chatAddress = await factory.chats(chatId);
+    expect(chatAddress).to.equal(ethers.constants.AddressZero);
+  });
+
+  it("Should create profile", async function () {
     const [signer] = await ethers.getSigners();
     const name = "test";
 
     await (
       await factory.createProfile(
         ethers.utils.formatBytes32String(name),
-        ethers.utils.randomBytes(44),
-        [],
-        []
+        ethers.utils.randomBytes(32)
       )
     ).wait();
 
@@ -48,5 +62,19 @@ describe("Factory", function () {
     expect(ethers.utils.parseBytes32String(await profile.name())).to.equal(
       name
     );
+  });
+
+  it("Should revert remove function if signer is not profile's owner", async function () {
+    const [, signer] = await ethers.getSigners();
+    await expect(factory.connect(signer).removeProfile()).to.be.reverted;
+  });
+
+  it("Should remove profile", async function () {
+    const [signer] = await ethers.getSigners();
+
+    await (await factory.removeProfile()).wait();
+
+    const profileAddres = await factory.profiles(signer.address);
+    expect(profileAddres).to.equal(ethers.constants.AddressZero);
   });
 });
